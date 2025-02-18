505e00b4070899bf1940f726c25bd92375266157
alexandrapetrea1 <alexandra.petrea1@stud.ubbcluj.ro>
2025-02-18 11:11:14 +0200

    subpunctul a)
    am facut subpunctul a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String file = "src/main/resources/ninja_events.tsv";
+        List<Map<String, String>> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String headerLine = br.readLine();
+            while (headerLine == null) {
+                System.out.println("Fisierul este gol");
+                return;
+            }
+            String[] headers = headerLine.split("\t");
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split("\t");
+                Map<String, String> row = new HashMap<>();
+                for (int i = 0; i < headers.length; i++) {
+                    row.put(headers[i], values[i]);
+                }
+                data.add(row);
+            }
+        } catch (IOException e) {
+            System.out.println("Eroare la citirea fisierului" + e.getMessage());
         }
+
+        System.out.println("Datele citite din fisierul TSV: ");
+        for (Map<String, String> row : data) {
+            System.out.println(row);
+        }
+
     }
 }
